8cb340a40b596567deb47bc5c7a783be916f37af
AlbertoAraiza <alberto.araiza88@gmail.com>
2020-02-26 15:13:49 -0600

    Testing Room
    Signed-off-by: AlbertoAraiza <alberto.araiza88@gmail.com>

--- app/src/main/java/mx/oaraiza/demodebs/database/DebtEntity.kt
+++ app/src/main/java/mx/oaraiza/demodebs/database/DebtEntity.kt
@@ -3,12 +3,11 @@ package mx.oaraiza.demodebs.database
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.sql.Date
-import java.sql.Timestamp
 
 @Entity
 class DebtEntity (
     @PrimaryKey(autoGenerate = true) var id: Int = 0,
     var nombre: String,
     var cantidad: Float,
-    var fecha : Date = Date(System.currentTimeMillis())
+    var fecha : Date? = Date(System.currentTimeMillis())
 )
